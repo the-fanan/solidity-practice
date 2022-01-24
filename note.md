da441c9749cb6b11ef196d82dcd1c3126e101b46
Fanan Dala <fanan.dala@yahoo.com>
2022-01-24 19:13:14 +0100

    build: add setup for deploying to other networks

--- note.md
+++ note.md
@@ -2,3 +2,16 @@ GAPS STILL LEFT TO FILL
 ==============================
 1. Calulating gas price and showing estimate to users
 2. Where to get current gas price info from
+
+
+
+RUNNING TESTS WITH TRUFFLE
+=============================
+- run `truffle developer` in the terminal
+- enter `test` in the integrated terminal that comes up
+
+
+DEPLOYING
+===============
+- run `trfulle migrate --netowork NETWORK_NAME --reset`
+- if no **NETWORK_NAME** is not specified the `development` network will be used
--- tutorials/Ethereum BlockChain Developer Bootcamp With Solidity/Understanding Web3 And Browser/fungible-token/truffle.js
+++ tutorials/Ethereum BlockChain Developer Bootcamp With Solidity/Understanding Web3 And Browser/fungible-token/truffle.js
@@ -1,14 +1,26 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require('dotenv');
+const mnemonic = process.env.HD_WALLET_MNEMONIC;
+const accountIndex = process.env.ACCOUNT_INDEX;
+const providerURI = process.env.BLOCKCHAIN_HTTP_PROVIDER_URI;
+
 module.exports = {
     networks: {
         development: {
             host: "127.0.0.1",
             port: 7545,
-            network_id: "5777"
+            network_id: "5777",
+        },
+        ropsten: {
+            provider: function () {
+                return new HDWalletProvider(mnemonic, providerURI, accountIndex);
+            },
+            network_id: "5777",
         }
     },
     compilers: {
         solc: {
-            version: "^0.8.0"
+            version: "^0.8.0",
         }
     },
     test_directory: './tests/contracts',
